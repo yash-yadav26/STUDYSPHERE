2e0af8d9624b4c8dfbbd48a875e2fd71e97a8c65
Yash Yadav <yashyadav73000@gmail.com>
2026-06-23 15:41:26 +0530

    final backend

--- Backend/Routes/invooiceRoutes.js
+++ Backend/Routes/invooiceRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../Middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
   createInvoice,
--- Backend/Routes/reportRoutes.js
+++ Backend/Routes/reportRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const authMiddleware = require("../Middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const { getDashboardStats } = require("../Controller/reportController");
 
